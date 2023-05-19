818fae71e8e61aec817198b4a946007350b038e2
Felicitaz Daut <feli.daut@concedra.de>
2023-05-19 20:44:15 +0200

    zeug

--- frontend/src/lib/Components/Checkbox.svelte
+++ frontend/src/lib/Components/Checkbox.svelte
@@ -4,36 +4,46 @@
 
 	let settings = 'settings';
 	let checkboxes = [];
-	for (let index: number = 1; index <= 10; index++) {
-		window[settings + index] = {
+	export let numberOfChoices: number = 10;
+	export let checkboxClasses: string = 'w-1/4';
+	export let checkboxNames: any = [];
+	//console.log(checkboxNames[2].name);
+
+	/* check if checkboxNames is empty if not count */
+	if (checkboxNames.length > 0) {
+		numberOfChoices = checkboxNames.length;
+	}
+
+	for (let index: number = 1; index <= numberOfChoices; index++) {
+		let id = checkboxNames[index - 1] != undefined ? checkboxNames[index - 1].id || index : index;
+		window[settings + id] = {
 			event: 'hover',
-			target: `tooltip-${index}`,
+			target: `tooltip-${id}`,
 			placement: 'top'
 		};
 		checkboxes.push({
-			id: index,
-			name: `Checkbox ${index}`,
-			tooltip: `Tooltip ${index}`,
+			id: id,
+
+			name: ` ${checkboxNames[index - 1] != undefined ? checkboxNames[index - 1].name : '' || 'Checkbox ' + id}`,
+			tooltip: `${checkboxNames[index - 1] != undefined ? checkboxNames[index - 1].tooltip : '' || 'Tooltip' + id}`,
 			checked: false,
-			dataPopup: `tooltip-${index}`
+			dataPopup: `tooltip-${id}`
 		});
 	}
 </script>
 
-<div class="flex flex-row flex-wrap justify-center text-center w-1/2">
-	{#each checkboxes as checkbox (checkbox.id)}
-		<label
-			use:popup={window[settings + checkbox.id]}
-			class="flex items-center justify-center space-x-2 w-1/4 p-2 card-hover m-2 cursor-pointer text-center hover:bg-secondary-400
+{#each checkboxes as checkbox (checkbox.id)}
+	<label
+		use:popup={window[settings + checkbox.id]}
+		class="flex items-center justify-center space-x-2 {checkboxClasses} p-2 card-hover m-2 cursor-pointer text-center hover:bg-secondary-400
              {checkbox.checked ? 'bg-primary-500 outline outline-1 outline-white' : 'card'}"
-		>
-			<input type="checkbox" bind:checked={checkbox.checked} class="hidden" />
-			<p class="text-center p-2">{checkbox.name}</p>
-		</label>
-		<div class="dark:bg-slate-600 bg-white p-4" data-popup={checkbox.dataPopup}>
-			{checkbox.tooltip}
-			<!-- Append the arrow element -->
-			<div class="arrow dark:bg-slate-600 bg-white" />
-		</div>
-	{/each}
-</div>
+	>
+		<input type="checkbox" bind:checked={checkbox.checked} class="hidden" />
+		<p class="text-center p-2">{checkbox.name}</p>
+	</label>
+	<div class="dark:bg-slate-600 bg-white p-4" data-popup={checkbox.dataPopup}>
+		{checkbox.tooltip}
+		<!-- Append the arrow element -->
+		<div class="arrow dark:bg-slate-600 bg-white" />
+	</div>
+{/each}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -15,12 +15,7 @@
 
 <div class="container h-1/2 mx-auto flex justify-center items-center flex-wrap">
 	<div class="text-center w-full space-y-5 p-4">
-		<Heading
-			headerText={state == 'initial value' || state == 'start' ? 'Willkommen' : ''}
-			headerSubtext={state == 'initial value' || state == 'start'
-				? 'Welche Dateien willst du bearbeiten?'
-				: 'Welche funktionen möchtest du durchführen?'}
-		/>
+		<Heading headerText={state == 'initial value' || state == 'start' ? 'Willkommen' : ''} headerSubtext={state == 'initial value' || state == 'start' ? 'Welche Dateien willst du bearbeiten?' : 'Welche funktionen möchtest du durchführen?'} />
 		<hr class="border-top-gradient" />
 	</div>
 	{#if state == 'initial value' || state == 'start'}
@@ -110,17 +105,63 @@
 		</div>
 	{:else}
 		<div class="text-center w-full" transition:fly={{ y: 200, duration: 300 }}>
-			<p>
-				Durch einen hover über die möglichkeiten, erhälst du informationen über die jeweillige
-				Funktion
-			</p>
+			<p>current state: {state}</p>
+			<h3 class="p-4">Mögliche presets</h3>
 			<div class="w-full flex flex-wrap flex-row justify-center">
-				<Checkbox />
+				<Checkbox
+					checkboxNames={[
+						{
+							name: 'HIAT-Check',
+							tooltip: 'tooltip für HIAT-Check',
+							id: 'hiat-check'
+						},
+						{
+							name: 'GAT-Check',
+							tooltip: 'tooltip für GAT-Check',
+							id: 'gat-check'
+						},
+						{
+							name: 'Transkript-Check',
+							tooltip: 'tooltip für Transkript-Check',
+							id: 'transkript-check'
+						},
+						{
+							name: 'Korpus-Check',
+							tooltip: 'tooltip für Korpus-Check',
+							id: 'korpus-check'
+						},
+						{
+							name: 'Metadaten-Check',
+							tooltip: 'tooltip für Metadaten-Check',
+							id: 'metadaten-check'
+						},
+						{
+							name: 'HTML-Ansichten',
+							tooltip: 'tooltip für HTML-Ansichten',
+							id: 'html-ansichten'
+						}
+					]}
+					checkboxClasses="w-1/8"
+				/>
 			</div>
-			<p>Hier findest kannst du auswählen was "gefixt" werden soll</p>
+			<hr class="m-4" />
+			<p>Durch einen hover über die möglichkeiten, erhälst du informationen über die jeweillige Funktion</p>
+
 			<div class="w-full flex flex-wrap flex-row justify-center">
-				<Checkbox />
+				<div class="flex flex-row flex-wrap justify-center text-center w-1/2">
+					<Checkbox numberOfChoices="15" />
+				</div>
 			</div>
+			<!-- 			<p>Hier findest kannst du auswählen was "gefixt" werden soll</p>
+			<div class="w-full flex flex-wrap flex-row justify-center">
+				<div class="flex flex-row flex-wrap justify-center text-center w-1/2">
+					<Checkbox numberOfChoices="10" />
+				</div>
+			</div> -->
+		</div>
+		<div class="w-full flex flex-wrap flex-row justify-center p-4 space-x-2">
+			<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all duration-300" on:click={() => pickedState.set('start')}> Zurück </button>
+			<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all duration-300" on:click={() => pickedState.set('stage 2')}> Weiter </button>
 		</div>
 	{/if}
 </div>
